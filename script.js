21e1611eeccdd4e04be82cd56109aabefe6b8400
Dav1BP <daviprado55@gmail.com>
2024-10-15 21:50:10 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 function lembrete(e){
     const ul = document.querySelector("ul")
     const li = document.createElement("li")
-
+    const inputs = document.querySelector(".inputs");
+        if(count <=20){
         if(document.querySelector(".inputs").value == ""){
              const inputs = document.querySelector(".inputs")
             inputs.setAttribute("placeholder","Lembrete invalido");
-        }else if(e.key == "Enter"){
-         const inputs = document.querySelector(".inputs").value;
-        li.append(inputs);
+        }else if(e.key == "Enter"){                                                         
+        li.append(inputs.value);
          ul.append(li);
+         inputs.value = "";
+         inputs.setAttribute("placeholder","Digite");
+         count++;
+         console.log(count)
         }
-
+    }else{
+        inputs.setAttribute("placeholder","num.max de lembrete");
+    }
 }
-
+let count = 0;
 
 const inputs = document.querySelector(".inputs")
 
